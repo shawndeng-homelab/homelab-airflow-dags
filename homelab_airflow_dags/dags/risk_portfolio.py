cac917fa7a820d32dd51b4ba8e14b525606a0540
ShawnDeng-code <shawndeng1109@qq.com>
2025-07-16 23:50:04 +0800

    ci: update docker file

--- homelab_airflow_dags/dags/risk_portfolio.py
+++ homelab_airflow_dags/dags/risk_portfolio.py
@@ -1,13 +1,14 @@
-# from datetime import datetime
+from datetime import datetime
 
-# from airflow import DAG
-# from airflow.operators.python import PythonOperator
+from airflow import DAG
+from airflow.operators.python import PythonOperator
 
 
-# def func():
-#     print("hello word")
+def func():
+    print("hello word")
 
-# with DAG(dag_id="test-dag",start_date=datetime(2025,7,12),schedule="@daily"):
-#     PythonOperator(
-#         python_callable=func,
-#     )
+with DAG(dag_id="test-dag",start_date=datetime(2025,7,12),schedule="@daily"):
+    PythonOperator(
+        task_id="test",
+        python_callable=func,
+    )
