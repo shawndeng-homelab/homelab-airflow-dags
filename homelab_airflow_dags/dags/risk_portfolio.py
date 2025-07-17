17d9a38e3458eed4ad4324d57e14d8b2f6b5dc76
colyerdeng <colyerdeng@tencent.com>
2025-07-17 13:41:36 +0800

    ci: update task file jobs and lint

--- homelab_airflow_dags/dags/risk_portfolio.py
+++ homelab_airflow_dags/dags/risk_portfolio.py
@@ -4,10 +4,11 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 
-def func():
+def func():  # noqa: D103
     print("hello word")
 
-with DAG(dag_id="test-dag",start_date=datetime(2025,7,12),schedule="@daily"):
+
+with DAG(dag_id="test-dag", start_date=datetime(2025, 7, 12), schedule="@daily"):
     PythonOperator(
         task_id="test",
         python_callable=func,
