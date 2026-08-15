bf733b6c1e4f1ebe3253cc1b7345d76fa78e1755
ShawnDeng-code <shawndeng1109@qq.com>
2026-08-15 14:38:55 +0800

    feat: export package version

--- packages/homelab-airflow-dags/src/homelab_airflow_dags/__init__.py
+++ packages/homelab-airflow-dags/src/homelab_airflow_dags/__init__.py
@@ -1,2 +1,9 @@
+from importlib.metadata import PackageNotFoundError, version
+
 __author__ = "Shawn Deng"
 __email__ = "shawndeng1109@qq.com"
+
+try:
+    __version__ = version("homelab-airflow-dags")
+except PackageNotFoundError:
+    __version__ = "0.0.0"
